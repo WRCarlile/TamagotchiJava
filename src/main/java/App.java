0422e5d11357f5ebf7b5f2ebcde80cd85198e492
Ian Lesikar and Ryan Carlile <ian@>
2016-04-26 16:45:46 -0700

    not work YET howerver index works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,36 +8,49 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+
+      get("/", (req, res) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        // model.put("tamas", request.session().attribute("tamas"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+      post("/tamas", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        String description = request.queryParams("description");
+        Tamas newTama = new Tamas(description);
+        request.session().attribute("tamas", newTama);
+        model.put("food", newTama.getFood());
+        model.put("mood", newTama.getMood());
+        model.put("energy", newTama.getEnergy());
+        model.put("health", newTama.getHealth());
+        model.put("isAlive", newTama.isAlive());
 
 
-    // staticFileLocation("/public");
-    //
-    //   get("/", (request, response) -> {
-    //     Map<String, Object> model = new HashMap<String, Object>();
-    //     model.put("tamas", request.session().attribute("tamas"));
-    //     model.put("template", "templates/index.vtl");
-    //     return new ModelAndView(model, "templates/layout.vtl");
-    //   }, new VelocityTemplateEngine());
-    //
-    //   post("/tamas", (request, response) -> {
-    //     Map<String, Object> model = new HashMap<String, Object>();
-    //
-    //     ArrayList<Tamas> tamas = request.session().attribute("tamas");
-    //     if (tamas == null) {
-    //       tamas = new ArrayList<Tamas>();
-    //       request.session().attribute("tamas", tamas);
-    //     }
-    //
-    //     String description = request.queryParams("description");
-    //     Tamas newTama = new Tamas(description);
-    //     tamas.add(newTama);
-    //     // request.session().attribute("tamas",newTama);
-    //
-    //     model.put("template", "templates/success.vtl");
-    //     return new ModelAndView(model, "templates/layout.vtl");
-    //   }, new VelocityTemplateEngine());
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+      post("/food", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        Tamas newTama = request.session().attribute("description");
+        newTama.getFood();
+        // request.session().attribute("tamas", );
+        Tamas newTama = new Tamas(description);
+        model.put("food", newTama.getFood());
+        model.put("mood", newTama.getMood());
+        model.put("energy", newTama.getEnergy());
+        model.put("health", newTama.getHealth());
+        model.put("isAlive", newTama.isAlive());
+
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+
   }
-
-
-
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,30 +1,30 @@
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-// import static org.fluentlenium.core.filter.FilterConstructor.*;
-// import java.util.ArrayList;
-//
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//
-//   @Override
-//   public WebDriver getDefaultDriver() {
-//     return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//   @Test
-//   public void rootTest() {
-//     goTo("http://localhost:4567/");
-//     assertThat(pageSource()).contains("Tamas You've Been");
-//   }
-//
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Tamas You've Been");
+  }
+
 //   @Test
 //   public void tamaIsCreatedTest() {
 //     goTo("http://localhost:4567/");
@@ -52,4 +52,4 @@
 //     assertThat(pageSource()).contains("Portland, OR");
 //     assertThat(pageSource()).contains("Seattle, WA");
 //   }
-// }
+}
